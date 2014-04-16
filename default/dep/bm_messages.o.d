ff8ba55bdf8c62fca4d68bb5c5fd3c2e41d8b739
sq2gxo <gxo@wp.pl>
2014-04-16 09:31:51 +0200

    Cleanup - remove not needed files

--- default/dep/bm_messages.o.d
+++
@@ -1,3 +0,0 @@
-bm_messages.o: ../bm_messages.c ../bm_messages.h
-
-../bm_messages.h:
--- default/dep/str_utils.o.d
+++
@@ -1,6 +0,0 @@
-str_utils.o: ../str_utils.c ../str_utils.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h
-
-../str_utils.h:
-
-c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
